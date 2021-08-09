eb5eaeb100782c71d6ae58e035b83f93e8326a7e
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-09 22:41:18 +0900

    [fix] 追加ページのレイアウト

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import TextBox from "./TextBox.jsx";
 import CustomCalendar from "./CustomCalender.jsx"
 import TextField from '@material-ui/core/TextField';
+import styles from './AddForm.module.css';
 
 export default function AddForm(props){
   const [title, setTitle] = useState('');
@@ -13,19 +14,24 @@ export default function AddForm(props){
     <div>
       <h1>Add form</h1>
       <TextBox value={title} onChange={(e) => setTitle(e.target.value)} />
-      <CustomCalendar
-        selectedDates={selectedDates}
-        setSelectedDates={setSelectedDates}
-      />
-      <TextField
-        label="備考"
-        multiline
-        rows={5}
-        value={memo}
-        variant="outlined"
-        fullWidth
-        onChange={(e) => setMemo(e.target.value)}
-      />
+      <div className={styles.mt}>
+        <CustomCalendar
+          selectedDates={selectedDates}
+          setSelectedDates={setSelectedDates}
+        />
+      </div>
+      <div className={styles.mtLarge}>
+        <TextField
+          label="備考"
+          multiline
+          minRows={10}
+          maxRows={10}
+          value={memo}
+          variant="outlined"
+          fullWidth
+          onChange={(e) => setMemo(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
