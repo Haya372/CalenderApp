1e2d6ecb5efac83257638c82da2c284a1338caa4
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-09 22:46:56 +0900

    [add] 提出ボタン

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -3,12 +3,18 @@ import TextBox from "./TextBox.jsx";
 import CustomCalendar from "./CustomCalender.jsx"
 import TextField from '@material-ui/core/TextField';
 import styles from './AddForm.module.css';
+import Button from '@material-ui/core/Button';
 
 export default function AddForm(props){
   const [title, setTitle] = useState('');
   const [selectedDates, setSelectedDates] = useState([new Date()]);
   const [tag, setTag] = useState('');
   const [memo, setMemo] = useState('');
+
+  const onClick = () => {
+    // 提出処理
+    console.log('提出します')
+  }
   
   return (
     <div>
@@ -31,6 +37,13 @@ export default function AddForm(props){
           fullWidth
           onChange={(e) => setMemo(e.target.value)}
         />
+      </div>
+      <div className={styles.buttonLayout}>
+        <Button
+          onClick={onClick}
+          variant="contained"
+          color="primary"
+        >作成</Button>
       </div>
     </div>
   )
