0d0f5d2fcdca4bec7f6921ebc804a99d7e3fc462
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-13 16:55:43 +0900

    [add] 予定表示用コンポーネント

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import ScheduleCalendar from "../components/ScheduleCalendar.jsx";
 
 export default function IndexPage(props){
   return (
     <div>
       This is index page
+      <ScheduleCalendar />
     </div>
   )
 }
