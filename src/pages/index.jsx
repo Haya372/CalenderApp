6cf07c714650e7d5c8dc804fdcd68748cbefadbd
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-15 01:18:09 +0900

    [add] サーバーでの認証失敗時にログインページに飛ばす

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ScheduleCalendar from "../components/ScheduleCalendar.jsx";
 import axios from "axios";
+import { useHistory } from "react-router";
 
 export default function IndexPage(props){
   const [schedules, setSchedules] = useState({});
   const [calendarBegin, setCalendarBegin] = useState(new Date());
+  const history = useHistory();
 
   useEffect(() => {
     const year = calendarBegin.getFullYear();
@@ -20,22 +22,7 @@ export default function IndexPage(props){
       setSchedules(data.schedules);
     }).catch((err) => {
       if(err.response.data === "Forbitton"){
-        // LIFF環境ではLIFFを使った処理に書き換える
-        const access_token = "test";
-        axios.post('/api/login', { access_token: access_token, user_id: localStorage.getItem('user_id') }).then((res) => {
-          axios.get('/api/schedules/', {
-            params: {
-              year: year,
-              month: month
-            }
-          }).then((res) => {
-            const data = res.data;
-            console.log(data)
-            setSchedules(data.schedules);
-          });
-        }).catch((err) => {
-          console.log(err.message);
-        });
+        history.push('/login');
       }
       console.log(err)
     });
