dc6271ee6722009a7ad63959e70b8e7207906182
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-14 18:15:46 +0900

    [add] /add => / の遷移

--- src/pages/add.jsx
+++ src/pages/add.jsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from "react";
 import AddForm from "../components/AddForm.jsx";
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Button from '@material-ui/core/Button';
+import { useHistory } from "react-router-dom";
 
 export default function AddPage(props){
+  const history = useHistory();
+
+  const onClick = () => {
+    history.push('/')
+  }
+
   return (
     <div>
-      This is Add page
-      <div>{props.user.name}</div>
+      <div>
+        <Button
+          startIcon={<ArrowBackIosIcon />}
+          variant="outlined"
+          color="primary"
+          onClick={onClick}
+        >
+          ホームへ戻る
+        </Button>
+      </div>
       <AddForm></AddForm>
     </div>
   )
