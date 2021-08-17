e35afe77cd8a1e43b7b63195c9a28aef28858a0c
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-17 15:51:55 +0900

    [add] ルーティング

--- src/components/ScheduleList.jsx
+++ src/components/ScheduleList.jsx
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import styles from './ScheduleList.module.css';
 import Divider from '@material-ui/core/Divider';
+import { useHistory } from 'react-router';
 
 const formatTime = (date) => {
   let hour = date.getHours();
@@ -13,14 +14,15 @@ const formatTime = (date) => {
 }
 
 export default function ScheduleList(props){
+  const history = useHistory();
+
   return (
     <div className={styles.wrapper}>
       {props.schedules.map((item) => {
 
         const onClick = (e) => {
           e.stopPropagation();
-          console.log(item.id, " is clicked");
-          // 各予定の編集画面へ遷移させる
+          history.push('/schedule/' + item.id);
         }
         
         const date = new Date(item.start_at);
