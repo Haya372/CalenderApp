8defc10d20a89919c6383b4eb1435349a6eb170d
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-18 14:47:47 +0900

    [add] カードのリストにアイコン表示

--- src/components/ScheduleList.jsx
+++ src/components/ScheduleList.jsx
@@ -4,6 +4,40 @@ import CardContent from '@material-ui/core/CardContent';
 import styles from './ScheduleList.module.css';
 import Divider from '@material-ui/core/Divider';
 import { useHistory } from 'react-router';
+import Avatar from '@material-ui/core/Avatar';
+import SchoolIcon from '@material-ui/icons/School';
+import SportsHandballIcon from '@material-ui/icons/SportsHandball';
+import WorkIcon from '@material-ui/icons/Work';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    marginBottom: "8px"
+  },
+  sports: {
+    backgroundColor: "#03a9f4",
+  },
+  work: {
+    backgroundColor: "#03a9f4",
+  },
+  hospital: {
+    backgroundColor: "red"
+  },
+  school: {
+    backgroundColor: "#03a9f4",
+  },
+  trip: {
+    backgroundColor: "#03a9f4",
+  },
+  important: {
+    backgroundColor: "red"
+  }
+}));
 
 const formatTime = (date) => {
   let hour = date.getHours();
@@ -15,6 +49,34 @@ const formatTime = (date) => {
 
 export default function ScheduleList(props){
   const history = useHistory();
+
+  const classes = useStyles();
+
+  const showIcon = (tag) => {
+    switch(tag){
+      case 'sports':
+        return <Avatar className={`${classes[tag]} ${classes.small}`}><SportsHandballIcon/></Avatar>;
+        break;
+      case 'work':
+        return <Avatar className={`${classes[tag]} ${classes.small}`}><WorkIcon /></Avatar>;
+        break;
+      case 'school':
+        return <Avatar className={`${classes[tag]} ${classes.small}`}><SchoolIcon /></Avatar>;
+        break;
+      case 'hospital':
+        return <Avatar className={`${classes[tag]} ${classes.small}`}><LocalHospitalIcon /></Avatar>;
+        break;
+      case 'trip':
+        return <Avatar className={`${classes[tag]} ${classes.small}`}><AirplanemodeActiveIcon /></Avatar>;
+        break;
+      case 'important':
+        return <Avatar className={`${classes[tag]} ${classes.small}`}><PriorityHighIcon /></Avatar>;
+        break;
+      default:
+        return <Avatar className={classes.small}><ImageIcon/></Avatar>;
+        break;
+    }
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -33,6 +95,7 @@ export default function ScheduleList(props){
               <CardContent>
                 <div className={styles.flexWrapper}>
                   <div className={styles.time}>
+                    {showIcon(item.tag)}
                     {formatTime(date)}<br />
                     ~
                   </div>
